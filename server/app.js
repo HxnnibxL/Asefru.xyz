8de493a96717f58dca3dae467de28252caf7b543
HxnnibxL <vinciidesign@gmail.com>
2024-12-11 23:30:33 +0100

    commit-5

--- server/app.js
+++ server/app.js
@@ -1,17 +1,20 @@
 const express = require("express");
-const app = express();
 const path = require("path");
 const XLSX = require("xlsx");
 
-const port = 3000;
+const app = express();
+
+// Utiliser le dossier public pour les fichiers statiques
 app.use(express.static("public"));
 
+// Route principale pour servir index.html
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/Index.html"));
 });
 
+// Route pour lire et retourner le fichier Excel en JSON
 app.get("/data", async function (req, res) {
-  const workbook = XLSX.readFile(__dirname + "/assets/poesy.xlsx");
+  const workbook = XLSX.readFile(path.join(__dirname, "assets/poesy.xlsx"));
   const sheetName = workbook.SheetNames[0];
   const worksheet = workbook.Sheets[sheetName];
   const dataToJson = XLSX.utils.sheet_to_json(worksheet);
@@ -19,6 +22,13 @@ app.get("/data", async function (req, res) {
   res.end(JSON.stringify(dataToJson));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// Vérifier si le script est exécuté en local
+if (require.main === module) {
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
+// Exporter l'application pour Vercel
+module.exports = app;
